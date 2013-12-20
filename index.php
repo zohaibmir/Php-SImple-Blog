614e55430eee73cdea7187b85ade9ca37b3ff805
zohaib <zohaib@zohaib-PC>
2013-12-21 00:57:42 +0100

    Update Index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>FaceTime Media Blog</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
+    <meta name="description" content="FaceTime Media Blog">
     <meta name="author" content="">
 
     <!-- Le styles -->
